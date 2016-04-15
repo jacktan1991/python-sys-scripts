aa451db3bee5e1404ada2221e262ea9583ce5cee
chen <chen@youjianghr.com>
2016-04-15 22:51:38 +0800

    feat:一个可以运行的脚步程序
    主要是为了体验click

---
+++ change_sys_datetime.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import click
+
+
+@click.command()
+def change_with_detail():
+    """
+    Quick python script to change system date and time.\n
+    More friendly compare to Unix `date` command
+    """
+    print "TODO:unimplement!!"
+
+if __name__ == '__main__':
+    change_with_detail()
